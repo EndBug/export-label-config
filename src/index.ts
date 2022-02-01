f8f7868bd68df7201a85d07e7aefd0651a2d6765
Federico Grandi <fgrandi30@gmail.com>
2022-02-01 12:02:31 +0100

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,14 @@ const { endGroup, getInput, startGroup } = core
 const log = {
   info: (str: string) => core.info('🛈 ' + str),
   success: (str: string) => core.info('✓ ' + str),
-  warning: (str: string, showInReport = true) => core[(showInReport ? 'warning' : 'info')]('⚠ ' + str),
-  error: (str: string, showInReport = true) => core[(showInReport ? 'error' : 'info')]('✗ ' + str),
+  warning: (str: string, showInReport = true) =>
+    core[showInReport ? 'warning' : 'info']('⚠ ' + str),
+  error: (str: string, showInReport = true) =>
+    core[showInReport ? 'error' : 'info']('✗ ' + str),
   fatal: (str: string) => core.setFailed('✗ ' + str)
-};
+}
 
-(async () => {
+;(async () => {
   try {
     checkInputs()
 
@@ -23,15 +25,20 @@ const log = {
 
     await uploadResult(labels)
 
-    log.success('Upload complete! You can find the results in the artifacts of this workflow run.')
+    log.success(
+      'Upload complete! You can find the results in the artifacts of this workflow run.'
+    )
   } catch (e) {
-    log.fatal(e+'')
+    log.fatal(e + '')
   }
 })()
 
 function checkInputs() {
   if (!getInput('token'))
-    log.warning('You\'re not passing any `token` option: if your repo is private the action will fail with a 404 error from the GitHub API.', false)
+    log.warning(
+      "You're not passing any `token` option: if your repo is private the action will fail with a 404 error from the GitHub API.",
+      false
+    )
 
   if (!['true', 'false'].includes(getInput('raw-result')))
     throw 'The only values you can use for the `raw-result` option are `true` and `false`'
@@ -53,19 +60,19 @@ async function fetchLabels() {
 
   const { data } = await axios.get(url, { headers })
   if (!data || !(data instanceof Array))
-    throw 'Can\'t get label data from GitHub API'
+    throw "Can't get label data from GitHub API"
 
   log.success(`${data.length} labels fetched.`)
   endGroup()
 
   return rawResult
-    ? data as Record<string, string | boolean>[]
-    : data.map(element => ({
-      name: element.name as string,
-      color: element.color as string,
-      description: element.description as string || undefined,
-      ...(addAliases ? { aliases: [] } : {})
-    }))
+    ? (data as Record<string, string | boolean>[])
+    : data.map((element) => ({
+        name: element.name as string,
+        color: element.color as string,
+        description: (element.description as string) || undefined,
+        ...(addAliases ? { aliases: [] } : {})
+      }))
 }
 
 async function uploadResult(labels: Record<string, any>[]) {
@@ -95,8 +102,7 @@ async function uploadResult(labels: Record<string, any>[]) {
     errors.push('yaml')
   }
 
-  if (errors.length >= 2)
-    log.fatal('Couldn\'t write any of the files.')
+  if (errors.length >= 2) log.fatal("Couldn't write any of the files.")
   else if (errors.length == 1)
     log.error(`Couldn't write ${errors[0].toUpperCase()} file.`)
 
@@ -105,26 +111,36 @@ async function uploadResult(labels: Record<string, any>[]) {
 
   // #region Artifact upload
   startGroup('Artifact upload')
-  const files = ['labels.json', 'labels.yaml'].filter(f => !f.endsWith(errors[0]))
-  log.info(`Uploading ${files.length} file${files.length == 1 ? '' : 's'}: ${files.join(', ')}`)
+  const files = ['labels.json', 'labels.yaml'].filter(
+    (f) => !f.endsWith(errors[0])
+  )
+  log.info(
+    `Uploading ${files.length} file${
+      files.length == 1 ? '' : 's'
+    }: ${files.join(', ')}`
+  )
 
   const response = await artifact
     .create()
-    .uploadArtifact('Label config', files.map(f => path.join(tempDir.name, f)), tempDir.name)
-    .catch(() => { throw 'Couldn\'t upload any file as artifact.' })
+    .uploadArtifact(
+      'Label config',
+      files.map((f) => path.join(tempDir.name, f)),
+      tempDir.name
+    )
+    .catch(() => {
+      throw "Couldn't upload any file as artifact."
+    })
 
   if (response) {
     log.info('Artifact result: ' + JSON.stringify(response, null, 2))
 
     if (response.failedItems.length >= files.length)
-      throw 'Couldn\'t upload any file as artifact.'
+      throw "Couldn't upload any file as artifact."
     else if (response.failedItems.length == 1)
       log.error(`Couldn't upload ${response.failedItems[0]} as artifact.`)
-    else
-      log.success('Successfully uploaded files.')
+    else log.success('Successfully uploaded files.')
   } else {
-    log.error('Can\'t read upload results.', false)
+    log.error("Can't read upload results.", false)
   }
   endGroup()
 }
-
